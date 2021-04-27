94b3f29cdd7827a044e2bd9905778bd4d641385d
tombeck <t@ocupop.com>
2021-04-27 12:14:49 -0700

    remove boostrap js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,3 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js"
-import "@popperjs/core/dist/umd/popper.min.js"
 import 'semantic-ui-css/semantic.min.css'
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import 'remixicon/fonts/remixicon.css'
@@ -8,4 +5,4 @@ import './src/styles/main.scss'
 
 
 import App from "./App"
-export const wrapRootElement = App;
+export const wrapRootElement = App
