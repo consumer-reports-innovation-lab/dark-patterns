6a448049c1ac47e0bc10dfa242cd62589e77f277
Nathaniel Fostan <nathanielfostan@gmail.com>
2021-04-30 11:55:28 -0500

    card styles

--- src/components/examples/ExampleCard.jsx
+++ src/components/examples/ExampleCard.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const ExampleCard = ({ example: { uri, date, patternAttributes: {affiliatedCompany, summary} } }) => {
+// const ExampleCard = ({ example: { uri, date, patternAttributes: {affiliatedCompany, summary} }, }) => {
+const ExampleCard = ({ example }) => {
+const {
+  uri,
+  title,
+  date,
+  categories,
+  patternAttributes: {
+    affiliatedCompany,
+    summary,
+    industry,
+  },
+  featuredImage: {
+    node: {
+      localFile
+    }
+  }
+}= example
+
 
   return (
     <Link to={uri} >
     <div className="card">
       <div className="card-body">
+        <div className="card-image">
+          <GatsbyImage image={getImage(localFile)} alt={title} />
+        </div>
         <p className="card-date">{date}</p>
-        <p className="card-title">{affiliatedCompany}</p>
+        <p className="card-company">{affiliatedCompany}</p>
+        <p className="card-title">{title}</p>
         <p className="card-text">{summary}</p>
         <p className="card-link">Explore Dark Pattern</p>
 
