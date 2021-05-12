ad072f2337c930c3eddf74ce21991f9f28ff7dcd
Nathaniel Fostan <nathanielfostan@gmail.com>
2021-05-12 18:24:11 -0500

    #13 updated footer

--- src/common/ui/menus/FooterMenu.jsx
+++ src/common/ui/menus/FooterMenu.jsx
@@ -7,31 +7,37 @@ const FooterMenu = () => {
   return (
     <div className="container">
       <div className="row py-5">
-        <nav className="navbar navbar-expand-md justify-content-md-between">
-          <StaticImage
+
+          {/* <StaticImage
             src="../../assets/global/cr-id-reversed.svg"
             alt="Consumer Reports"
             className="img-fluid "
             height={50}
-          />
-          <ul className="navbar-nav">
-            <li className="nav-item content-center text-end">
-              <span className="nav-link">
-                &copy; {new Date().getFullYear()} Consumer Reports, Inc
-              </span>
-            </li>
-            <li className="nav-item text-end">
-              <Link to="/how-we-use-submission" className="nav-link">
-                How We'll Use Your Submission
-              </Link>
-            </li>
-            <li className="nav-item text-end">
-              <a href="https://www.consumerreports.org/cro/2015/01/user-agreement/index.htm" className="nav-link" target="_blank">
-                User Agreement
-              </a>
-            </li>
+          /> */}
+          <div className="col-12 col-md-7 col-lg-4 d-flex align-items-center">
+            <span className="text-mono text-mid ">An experiment incubated by the CR Digital Lab</span>
+          </div>
+          <div className="col-12 col-md-5 col-lg-8">
+            <ul className="navbar-nav">
+              <li className="nav-item text-end">
+                <span className="nav-link">
+                  &copy; {new Date().getFullYear()} Consumer Reports, Inc
+                </span>
+              </li>
+              <li className="nav-item text-end">
+                <Link to="/how-we-use-submission" className="nav-link">
+                  How We'll Use Your Submission
+                </Link>
+              </li>
+              <li className="nav-item text-end">
+                <a href="https://www.consumerreports.org/cro/2015/01/user-agreement/index.htm" className="nav-link" target="_blank">
+                  User Agreement
+                </a>
+              </li>
           </ul>
-        </nav>
+          </div>
+
+
       </div>
     </div>
   );
