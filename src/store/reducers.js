4c8412ed18a27ecafa76491bf9f15083b2d0621a
tombeck <t@ocupop.com>
2021-05-13 07:21:44 -0700

    component cleanup

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import asyncReducer from '../common/async/asyncReducer'
-import modalReducer from '../common/modal/modalReducer'
 
 const rootReducer = () => combineReducers({
   async: asyncReducer,
-  modals: modalReducer,
 })
 
 export default rootReducer
