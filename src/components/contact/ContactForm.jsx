9ed251ca886d8cf3dce553a4403f919f4064f772
Nathaniel Fostan <nathanielfostan@gmail.com>
2021-05-17 21:51:28 -0500

    added contact email form #73

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -2,11 +2,61 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 import { Formik, Field, Form } from 'formik'
 import * as Yup from 'yup'
+import {
+  EmailInput,
+  TextArea,
+} from '../../common/fields'
 
 const ContactForm = () => {
 
   return (
     <>
+    <Formik
+       initialValues={{ email: '', password: '' }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+
+          <Field
+            name="optional_email"
+            type="text"
+            component={EmailInput}
+            placeholder="For outreach in case we have questions"
+            hint="You can optionally share your email so we can reach out if we have any questions. We won’t share your email."
+            className="mb-4"
+            label="Email (Optional)"
+          />
+
+          <Field
+            name="description"
+            type="text"
+            component={TextArea}
+            placeholder="Get in touch with us"
+            hint="Get in touch with us"
+          />
+
+           <button type="submit" className="btn btn-primary my-5">
+             Send
+           </button>
+
+         </form>
+       )}
+     </Formik>
     </>
   )
 }
