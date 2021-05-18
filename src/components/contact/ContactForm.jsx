45f0264eae0bf67d8d9225b57f84bf3e206b3df7
Nathaniel Fostan <nathanielfostan@gmail.com>
2021-05-17 21:34:51 -0500

    removed semantic ui styles #79

---
+++ src/components/contact/ContactForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { Formik, Field, Form } from 'formik'
+import * as Yup from 'yup'
+
+const ContactForm = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
+ContactForm.propTypes = {
+
+}
+
+export default ContactForm
