be869f4e6a90ff38b90743f61338fd19a47beb21
stephtngu <60265274+stephtngu@users.noreply.github.com>
2021-05-22 07:27:07 -0400

    Update Intro.jsx

--- src/components/examples/Intro.jsx
+++ src/components/examples/Intro.jsx
@@ -10,7 +10,7 @@ const Intro = () => {
           <div className="col-12 col-md-8 col-lg-7">
             <h1>Dark patterns<br />spotted by people<br />like you</h1>
             <p>
-              Frustrated people all over the world have found dark patterns even on mainstream websites and apps. Click around to see some examples of how these dark patterns hurt people, and better understand the techniques companies use to trick you into doing what they want.
+              Frustrated people all over the world have found dark patterns even on mainstream websites and apps. Click around to see some examples of how these dark patterns hurt people, and better understand the techniques companies use to trick you into doing what they want. These submissions reflect the views and experiences of the people who submitted them.
               </p>
             <Link to="/report" className="btn btn-primary">Report a Pattern</Link>
           </div>
